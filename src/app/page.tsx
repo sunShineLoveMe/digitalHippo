a80513d293226f793b61fc7d8c7cf42096cf3047
sunShineLoveMe <juneandrao@outlook.com>
2024-03-14 15:38:51 +0800

    finish the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,25 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import { Button, buttonVariants } from "@/components/ui/button";
+import { ArrowDownToLine, Check, CheckCircle, Leaf } from "lucide-react";
 import Link from "next/link";
+
+const perks = [
+  {
+    name: 'Instant Devliery',
+    Icon: ArrowDownToLine,
+    description: 'Get your assets deleivered instantly after purchase and download them right away'
+  },
+  {
+    name: 'Guaranteed Quality',
+    Icon: CheckCircle,
+    description: 'Every asset on our platform is carefully reviewed and curated by our team of experts'
+  },
+  {
+    name: 'For our Planet',
+    Icon: Leaf,
+    description: 'We plant a tree for every purchase made on our platform to help our planet'
+  }
+]
 
 export default function Home() {
   return (
@@ -25,7 +44,23 @@ export default function Home() {
       <section className="border-t border-gray-200 bg-gray-50">
         <MaxWidthWrapper className="py-20">
           <div className="grid grid-cols-1 gap-y-12 sm:gap-x-6 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-0">
-              
+            {perks.map((perk) => 
+              <div key={perk.name} className="text-center md:flex md:items-start md:text-left lg:block lg:text-center">
+                <div className="md:flex-shrink-0 flex justify-center">
+                  <div className="h-16 w-16 flex items-center justify-center rounded-full bg-blue-100 text-blue-900">
+                    { <perk.Icon className="w-1/3 h-1/3" /> }
+                  </div>
+                </div>
+                <div className="mt-6 md:ml-4 md:mt-0 lg:ml-0 lg:mt-6">
+                  <h3 className="text-base font-medium text-gray-900">
+                    {perk.name}
+                  </h3>
+                  <p className="mt-3 text-sm text-muted-foreground">
+                    {perk.description}
+                  </p>
+                </div>
+              </div>
+            )}
           </div>
         </MaxWidthWrapper>
       </section>
