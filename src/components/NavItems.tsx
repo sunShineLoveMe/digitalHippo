530d77dee1b5df7df78240c10c96931e5453ae86
sunShineLoveMe <juneandrao@outlook.com>
2024-03-15 12:53:16 +0800

    add shopping cart

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,15 +1,31 @@
 "use client";
 
 import { PRODUCT_CATEGORIES } from "@/config";
-import { useState } from "react";
+import { useState, useRef, use, useEffect } from "react";
 import NavItem from "./NavItem";
+import { useOnClickOutside } from "@/hooks/use-on-click-outside";
 
 const NavItems = () => {
     const [activeIndex, setActiveIndex] = useState<null | number>(null);
 
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) => {
+            if(e.key === 'Escape') {
+                setActiveIndex(null)
+            }
+        }
+        document.addEventListener("keydown", handler);
+        return () => {
+            document.removeEventListener("keydown", handler);
+        }
+    }, [])  
+
     const isAnyOpen = activeIndex !== null;
+
+    const navRef = useRef<HTMLDivElement>(null);
+    useOnClickOutside(navRef, () => setActiveIndex(null) )   
     return (
-        <div className="flex gap-4 h-full">
+        <div className="flex gap-4 h-full" ref={navRef}>
             {PRODUCT_CATEGORIES.map((category, i) => {
                 const handleOpen = () => {
                     if (activeIndex === i) {
---
+++ src/hooks/use-on-click-outside.ts
@@ -0,0 +1,30 @@
+/**
+ * 这段代码是用于检测用户是否点击页面中元素之外的区域。
+ */
+import { RefObject, useEffect } from "react";
+
+type Event = MouseEvent | TouchEvent;
+
+export const useOnClickOutside = <T extends HTMLElement = HTMLElement>(
+  ref: RefObject<T>,
+  handler: (event: Event) => void
+) => {
+  useEffect(() => {
+    const listener = (event: Event) => {
+      const el = ref?.current;
+      if (!el || el.contains((event?.target as Node) || null)) {
+        return;
+      }
+
+      handler(event); // Call the handler only if the click is outside of the element passed.
+    };
+
+    document.addEventListener("mousedown", listener);
+    document.addEventListener("touchstart", listener);
+
+    return () => {
+      document.removeEventListener("mousedown", listener);
+      document.removeEventListener("touchstart", listener);
+    };
+  }, [ref, handler]); // Reload only if ref or handler changes
+};
