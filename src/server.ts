3e231b1c8b00b1ab9f1e019643b500dc456ea4fe
sunShineLoveMe <juneandrao@outlook.com>
2024-03-20 11:30:43 +0800

    trpc combine next

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
-import express from "express";
+import express, { application } from "express";
 import { getPayloadClient } from "./get-payload";
 import { nextApp, nextHandler } from "./next-utils";
+import * as trpcExpress from "@trpc/server/adapters/express";
+import { appRouter } from "./trpc";
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
+const createContext = ({req, res} : trpcExpress.CreateExpressContextOptions) => ({
+    req, res
+})
 
 const start = async () => {
     const payload = await getPayloadClient({ 
@@ -15,6 +20,10 @@ const start = async () => {
         } 
     });
 
+    app.use("/api/trpc", trpcExpress.createExpressMiddleware({
+        router: appRouter,
+        createContext
+    }))
     app.use((req, res) => nextHandler(req, res))
 
     nextApp.prepare().then(() => {
